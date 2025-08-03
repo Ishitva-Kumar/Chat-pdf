521751cd0a5ce6a40b5ee771a3ca6ca552222dd1
Ishitva Kumar <ishitvakumar2003@gmail.com>
2025-08-03 14:53:22 +0530

    updated login page ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,9 @@ import { login, signup } from './actions'
 
 export default function LoginPage() {
   return (
-    <form>
+    <div className='flex flex-col space-y-6 items-center justify-center h-[80svh]'>
+        <h1 className='text-lg'>Get Started</h1>
+    <form className='flex flex-col space-y-4 w-full md:w-xl border-[1px] px-6 py-8'>
       <label htmlFor="email">Email:</label>
       <input id="email" name="email" type="email" required />
       <label htmlFor="password">Password:</label>
@@ -10,5 +12,6 @@ export default function LoginPage() {
       <button formAction={login}>Log in</button>
       <button formAction={signup}>Sign up</button>
     </form>
+    </div>
   )
 }
